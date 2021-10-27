016ed78c0730b9337658dc9d2a43ce55865ea8f9
tchartchke <chartai.kl@gmail.com>
2021-10-27 15:53:06 -0600

    done

--- cli.py
+++ cli.py
@@ -1,65 +1,75 @@
-board = ['-'] * 9
-positions = list(range(1, 10))
+class PycPacPoe:
+  WIN_COMBOS = ( 
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [2, 4, 6] 
+  )
+  def __init__(self):
+    self.board = [' '] * 9
 
-winningCombos = ( 
-  [0, 1, 2], 
-  [3, 4, 5], 
-  [6, 7, 8], 
-  [0, 3, 6], 
-  [1, 4, 7], 
-  [2, 5, 8], 
-  [0, 4, 8], 
-  [2, 4, 6] 
-)
+  def display_board(self):
+    print(f" {self.board[0]} | {self.board[1]} | {self.board[2]} \n-----------\n {self.board[3]} | {self.board[4]} | {self.board[5]} \n-----------\n {self.board[6]} | {self.board[7]} | {self.board[8]} ")
 
-def printBoard(b):
-  print(f" {b[0]} | {b[1]} | {b[2]} \n {b[3]} | {b[4]} | {b[5]} \n {b[6]} | {b[7]} | {b[8]} ")
+  def input_to_index(self, input):
+    return int(input) - 1
+
+  def move(self, index, token = 'X'):
+    self.board[index] = token
+
+  def is_position_taken(self, index):
+    return False if self.board[index] == ' ' else True
+
+  def is_valid_move(self, index):
+    return index in range(0,9) and not self.is_position_taken(index)
+  
+  def turn(self):
+    position = self.input_to_index(input("Input position (1-9): "))
+    while not self.is_valid_move(position):
+      print("Not a valid position. Please try again.")
+      position = self.input_to_index(input("Input position (1-9): "))
+
+    print("before move")
+    self.move(position, self.current_player())
+    self.display_board()
+
+  def turn_count(self):
+    return 9 - self.board.count(' ')
+
+  def current_player(self):
+    return 'X' if self.turn_count() % 2 == 0 else 'O'
+
+  def is_won(self):
+    for combo in self.WIN_COMBOS:
+      if self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]] != ' ':
+        return True
+    return False
+
+  def is_full(self):
+    return ' ' not in self.board
+
+  def is_draw(self):
+    return self.is_full() and not self.is_won()
+
+  def is_over(self):
+    return self.is_draw() or self.is_won()
+
+  def winner(self):
+    return 'X' if self.turn_count() % 2 == 1 else 'O'
+
+  def play(self):
+    while not self.is_over():
+      self.turn()
+    print("Game Complete")
+    if self.is_won(): 
+      print(f"Congrats {self.winner()}")
+    else:
+      print("Tie")
 
 
-def spaceOpen(position):
-  return True if board[position] == '-' else False
-
-
-def updateBoard(position, symbol):
-    board[position] = symbol 
-
-
-def getMove():
-  position = input("Select position: ")
-  try:
-    return int(position)
-  except: 
-    print("Not a number")
-
-
-def validMove(input):
-  return True if input in positions and spaceOpen(input-1) else False
-
-
-def takeTurn(symbol):
-  position = getMove()
-  while not position:
-    position = getMove()
-  if spaceOpen(position):
-    updateBoard(position, symbol)
-
-
-def gameIsWon():
-  for combo in winningCombos:
-    if not spaceOpen(combo[0]) and board[combo[0]] == board[combo[1]] == board[combo[2]]:
-      return board[combo[0]]
-  return False
-
-
-def play():
-  '''
-  get player 1
-  get player 2
-  while no win
-    play rounds
-  congratulate winner
-  '''
-
-  p1 = input("Player 1 symbol: ")
-  p2 = input("Player 2 symbol: ")
-
+game = PycPacPoe()
+game.play()
