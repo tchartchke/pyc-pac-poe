9525acbb2184b257531419534006b0a3f0680862
tchartchke <chartai.kl@gmail.com>
2021-10-28 08:24:33 -0600

    expanded board

--- 3dcli.py
+++ 3dcli.py
@@ -1,7 +1,22 @@
 class ThrycThracThroe:
-  WIN_COMBOS = ()
+  WIN_COMBOS = (
+   
+  )
 
   def __init__(self):
-    self.board = [[' '] * 9] * 3
+    self.board = [[['x'] * 4] * 4] * 4
+
+  def display_board(self):
+    format = f"""
+ {self.board[0][0][0]} | {self.board[0][0][1]} | {self.board[0][0][2]} | {self.board[0][0][3]}      {self.board[1][0][0]} | {self.board[1][0][1]} | {self.board[1][0][2]} | {self.board[1][0][3]}      {self.board[2][0][0]} | {self.board[2][0][1]} | {self.board[2][0][2]} | {self.board[2][0][3]} 
+---------------    ---------------    ---------------
+ {self.board[0][1][0]} | {self.board[0][1][1]} | {self.board[0][1][2]} | {self.board[0][1][3]}      {self.board[1][1][0]} | {self.board[1][1][1]} | {self.board[1][1][2]} | {self.board[1][1][3]}      {self.board[2][1][0]} | {self.board[2][1][1]} | {self.board[2][1][2]} | {self.board[2][1][3]} 
+---------------    ---------------    ---------------
+ {self.board[0][2][0]} | {self.board[0][2][1]} | {self.board[0][2][2]} | {self.board[0][2][3]}      {self.board[1][2][0]} | {self.board[1][2][1]} | {self.board[1][2][2]} | {self.board[1][2][3]}      {self.board[2][2][0]} | {self.board[2][2][1]} | {self.board[2][2][2]} | {self.board[2][2][3]} 
+---------------    ---------------    ---------------
+ {self.board[0][3][0]} | {self.board[0][3][1]} | {self.board[0][3][2]} | {self.board[0][3][3]}      {self.board[1][3][0]} | {self.board[1][3][1]} | {self.board[1][3][2]} | {self.board[1][3][3]}      {self.board[2][3][0]} | {self.board[2][3][1]} | {self.board[2][3][2]} | {self.board[2][3][3]} """
+    print(format)
 
 game = ThrycThracThroe()
+
+game.display_board()
